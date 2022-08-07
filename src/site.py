da4ce0980726acdc82296684b118268c9b780790
Angelollima <angeloantonio442@gmail.com>
2022-08-07 00:23:24 -0300

    Modifying codes

--- src/site.py
+++ src/site.py
@@ -1,8 +1,11 @@
+from ast import pattern
 import code
+from crypt import methods
 from urllib import response
 from flask import Flask, render_template, request
 from math import sqrt
 from operator import add, sub, mul, pow
+import re
 
 app = Flask(__name__)
 
@@ -20,13 +23,21 @@ operacoes = {
 def main() -> None:
     if request.method == "GET":
         return render_template("index.html")
-    else:
-        valor1 = int(request.form.get())
-        operacao = request.form.get()
-        valor2 = int(request.form.get())
-        if operacao in operacoes:
-            final = operacoes[operacao](valor1, valor2)
-            return render_template("index.html", final=final)
+
+@app.route('/conta/<string:operacao>', methods=["POST"])
+def receive(operacao):
+    print(operacao)
+    pattern = r'(\w+)'
+    l = re.split(pattern, operacao)
+    l = re.sub(r"^\s+|\s+$", "", operacao)
+    print(l)
+    valor1 = float(l[0])
+    operacao_final = l[1]
+    valor2 = float(l[2])
+    if operacao_final in operacoes:
+        final = operacoes[operacao_final](valor1, valor2)
+        print(final)
+        return final
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,6 +1,40 @@
-var input = document.getElementById('tela'),
-numero = document.querySelectorAll('.numeros div'),
-conta = document.querySelectorAll('.contas div'),
-resultado = document.getElementById('resultado'),
-limpar = document.getElementById('limpar'),
-resultadoEstaMostrado = false;
+var str = '';
+const tela = document.getElementById("tela");
+
+$(".opas").on("click", function() {
+    str += this.id;
+    tela.append(str.slice(-1));
+});
+
+$(".operacoes").on("click", function() {
+    operacoes = ["+", "-", "*", "/", "%"];
+    if (operacoes.includes(tela.textContent.slice(-1))){
+        alert("burro");
+    } else {
+        str += this.id;
+        tela.append(str.slice(-1));
+    }
+});
+
+$("#igual").on("click", function() {
+    if (!isNaN(tela.textContent.slice(-1))){
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:5000/conta/" + str
+        });
+        tela.innerHTML = "";
+        str = '';
+    } else {
+        alert("burro");
+    }
+});
+
+$("#limpar").on("click", function() {
+    str = '';
+    tela.innerHTML = "";
+});
+
+$.ajax({
+    method: "POST",
+    url: "http://localhost:5000/" + str
+});
